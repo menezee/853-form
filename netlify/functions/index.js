75d19f00efa7f750802a0efb188232064a66dd90
Erich Menezes <erich.menezes1@gmail.com>
2022-10-17 23:11:01 -0300

    v0.14

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -25,7 +25,7 @@ function buildHtml(pokemon, allParams) {
               All params:
               <br/>
               <ul>
-                ${allParams.map(([k, v]) => `<li>${k} → ${v}</li>`).join('')}
+                ${allParams.map(([k, v]) => `<li>${k} - ${v}</li>`).join('')}
               </ul>
             </main>
           </body>
@@ -67,50 +67,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-// // const axios = require('axios').default;
-// import fetch from 'node-fetch';
-
-// const API_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/';
-
-// exports.handler = async (event, context) => {
-//   try {
-//     const { pokemonID } = event.body;
-//     const requestURL = `${API_ENDPOINT}${pokemonID}`;
-//     console.log('[DEBUG]', JSON.stringify({ pokemonID, requestURL }));
-//     // const { data: pokemon } = await axios.get(`${API_ENDPOINT}${pokemonID}`);
-//     const res = await fetch(requestURL);
-//     const pokemon = await res.json();
-//     console.log('[DEBUG]', JSON.stringify({ pokemonName: pokemon.name }));
-//     const html = `
-//       <!DOCTYPE html>
-//       <html>
-//         <head>
-//           <title>${pokemon.name}</title>
-//         </head>
-//         <body>
-//           <header>
-//             <h1>${pokemon.name}</h1>
-//           </header>
-//           <main>
-//             <img src="${pokemon.sprites.front_default}" />
-//             <ul>
-//               ${pokemon.abilities.map(data => (
-//                 `<li>${data.ability.name}</li>`
-//               ))}
-//             </ul>
-//           </main>
-//         </body>
-//       </html>
-//     `;
-//     return {
-//       statusCode: 200,
-//       body: html,
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ err: 'Failed fetching data', error }),
-//     };
-//   }
-// };
